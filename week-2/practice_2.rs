653006643d75969758aaaf1fb2b925dd93e30af2
oluwadara08 <oluwadara.akinfenwa@pau.edu.ng>
2025-10-10 16:33:33 +0100

    (My Third Commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,12 @@
+fn main(){
+
+	println!(); // prints just a line
+ 
+	println!("hello");
+
+	println!("format {} argumets", "some"); // prints formats some arguments
+
+	println!("My name is {}. I am the class rep of stream 1", "Dagogo William-Jumbo");
+
+}
+
