3424168a06455a22622f4ad941d505bc86db0b1c
oluwadara08 <oluwadara.akinfenwa@pau.edu.ng>
2025-10-15 13:54:02 +0100

    (Commit)

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_character = '@'; // default
+    let alphabet:char = '8';
+    let surname = "Bolaji";
+    let first_name = "Micheal";
+    let middle_name = "Gboyega";
+
+    println!(" ");
+    println!("Special character: {}",special_character);
+    println!("ALphabet: {}", alphabet);
+    println!("Name: {} {} {}",surname, middle_name, first_name);
+}
---
+++ week-3/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\TOSHIBA\ PC\Documents\o.akinfenwaCOS101\week-3\practice_5\target\debug\practice_5.exe: C:\Users\TOSHIBA\ PC\Documents\o.akinfenwaCOS101\week-3\practice_5\src\main.rs
