8e3b75475ca76959a9bea9e30f6e00195b7182a8
oluwadara08 <oluwadara.akinfenwa@pau.edu.ng>
2025-10-15 20:01:38 +0100

    (Commit)

---
+++ week-3/practice_10/target/debug/deps/practice_10.d
@@ -0,0 +1,5 @@
+C:\Users\TOSHIBA PC\Documents\o.akinfenwaCOS101\week-3\practice_10\target\debug\deps\practice_10.d: src\main.rs
+
+C:\Users\TOSHIBA PC\Documents\o.akinfenwaCOS101\week-3\practice_10\target\debug\deps\practice_10.exe: src\main.rs
+
+src\main.rs:
