d2eb71ed2d36badecd2b5b16acb37440401e0a0e
oluwadara08 <oluwadara.akinfenwa@pau.edu.ng>
2025-10-22 21:11:36 +0100

    (Eighth)

---
+++ week-4/practice_8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\TOSHIBA PC\Documents\o.akinfenwaCOS101\week-4\practice_8\target\debug\deps\practice_8.d: src\main.rs
+
+C:\Users\TOSHIBA PC\Documents\o.akinfenwaCOS101\week-4\practice_8\target\debug\deps\practice_8.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\TOSHIBA\ PC\Documents\o.akinfenwaCOS101\week-4\practice_8\target\debug\practice_8.exe: C:\Users\TOSHIBA\ PC\Documents\o.akinfenwaCOS101\week-4\practice_8\src\main.rs
