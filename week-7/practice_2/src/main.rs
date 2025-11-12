7067fed80575adecc63a5331b9dc90fe2a9f5370
oluwadara08 <oluwadara.akinfenwa@pau.edu.ng>
2025-11-12 13:45:04 +0100

    (2nd)

---
+++ week-7/practice_2/src/main.rs
@@ -0,0 +1,25 @@
+use std::io;
+
+fn checker(){
+
+    let mut input = String::new();
+    println!("Enter a character:");
+    io::stdin().read_line(&mut input).expect("failed to read input");
+    let ch:char = input.trim().parse().expect("Invalid input");
+
+    if ch >= '0' && ch <= '9'
+    {
+        println!("character '{}' is a digit", ch);
+    }
+    else
+    {
+        println!("character '{}' is not a digit", ch);
+    }
+}
+
+fn main(){
+    //calling function
+    println!("Welcome! this program checks whether a character
+        variable contains a digit or not");
+    checker()
+}
