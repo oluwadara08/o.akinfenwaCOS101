5094311d59006b92decb411d1106ea50fa6c8ca3
oluwadara08 <oluwadara.akinfenwa@pau.edu.ng>
2025-11-16 16:20:52 +0100

    (ye)

---
+++ week-7/practice_8/src/main.rs
@@ -0,0 +1,10 @@
+fn main(){
+
+    let city_arr:[&str;5] = ["abuja","port harcourt","maiduguri","kano","lagos"];
+    println!("array is {:?}",city_arr);
+    println!("array size is: {}",city_arr.len());
+
+    for index in 0..5{
+        println!("City index {:?} is located in : {}",index,city_arr[index]);
+    }
+}
---
+++ week-7/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\TOSHIBA\ PC\Documents\o.akinfenwaCOS101\week-7\practice_8\target\debug\practice_8.exe: C:\Users\TOSHIBA\ PC\Documents\o.akinfenwaCOS101\week-7\practice_8\src\main.rs
